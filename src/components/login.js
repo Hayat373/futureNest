7eeddee32616970cf1fd422749f05b1e1907f5b0
Hayat Ahmedjara <hayahmam3@gmail.com>
2025-06-19 07:17:14 -0700

    login-page

--- src/components/login.js
+++ src/components/login.js
@@ -1,9 +1,23 @@
 import React from "react";
+import '../css/login.css'
 
 const Login=()=>{
     return(
-        <div>
+        <div className="conatiner">
+
+            <h1>Login</h1>
+            <div className="form_group field">
+    <input type="text" className="form_field" placeholder="User Name" required />
+    <label htmlFor="name" className="form_label">User Name</label>
+    <br/>
+    <input id="passwordinput"type="password" className="form_field" placeholder="password" required />
+    <label id="password" htmlFor="password" className="form_label">Password</label>
+
+</div>
+           
             
         </div>
     )
-}
+};
+
+export default Login;
--- src/page/loginpage.js
+++ src/page/loginpage.js
@@ -1,11 +1,13 @@
 import React from "react";
 import Header from "../components/header";
 import '../css/loginpage.css';
+import Login from "../components/login";
 
 const Loginpage=()=>{
     return(
         <div>
         <Header/>
+        <Login />
         </div>
     )
 }
