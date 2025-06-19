4ad5956f20f991960f2418818894a9af7a7ec061
Hayat Ahmedjara <hayahmam3@gmail.com>
2025-06-19 11:41:44 -0700

    login-page-button

--- src/components/login.js
+++ src/components/login.js
@@ -10,10 +10,17 @@ const Login=()=>{
     <input type="text" className="form_field" placeholder="User Name" required />
     <label htmlFor="name" className="form_label">User Name</label>
     <br/>
-    <input id="passwordinput"type="password" className="form_field" placeholder="password" required />
+    <input  id="passwordinput"type="password" className="form_field" placeholder="password" required />
     <label id="password" htmlFor="password" className="form_label">Password</label>
 
 </div>
+
+ <div className="links">
+                <a href="#" className="link">Forgot Password?</a>
+                <a  id="sign" href="#" className="link">Sign Up</a>
+            </div>
+
+             <button className="submit_button">Login</button>
            
             
         </div>
