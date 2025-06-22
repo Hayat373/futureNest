dc906feb50c1e5e54bf0ba75839e2a175fe951cc
Hayat Ahmedjara <hayahmam3@gmail.com>
2025-06-22 10:00:14 -0700

    img added to signup and login

--- src/components/login.js
+++ src/components/login.js
@@ -1,9 +1,39 @@
-import React from "react";
+import React, { useState } from "react";
+import { Link, useNavigate } from 'react-router-dom';
 import '../css/login.css'
 
 const Login=()=>{
+
+     const [profileImage, setProfileImage] = useState(null);
+    
+        const handleImageUpload = (e) => {
+            const file = e.target.files[0];
+            if (file) {
+                const imageUrl = URL.createObjectURL(file);
+                setProfileImage(imageUrl);
+            }
+        };
+
     return(
         <div className="conatiner">
+            <div className="profile-plate">
+                <div className="profile-circle">
+                    {profileImage ? (
+                        <img src={profileImage} alt="Profile" className="profile-image" />
+                    ) : (
+                        <div className="profile-placeholder"></div>
+                    )}
+                    <label className="plus-icon">
+                        <span>+</span>
+                        <input
+                            type="file"
+                            accept="image/*"
+                            onChange={handleImageUpload}
+                            style={{ display: 'none' }}
+                        />
+                    </label>
+                </div>
+            </div>
 
             <h1>Login</h1>
             <div className="form_group field">
@@ -17,7 +47,9 @@ const Login=()=>{
 
  <div className="links">
                 <a href="#" className="link">Forgot Password?</a>
-                <a  id="sign" href="#" className="link">Sign Up</a>
+               <Link to="/signup" className="link" id="signup">
+          Sign Up
+        </Link>
             </div>
 
              <button className="submit_button">Login</button>
