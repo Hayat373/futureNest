cbfa00f08b2ca35eafcbbcad913c8768df367d70
Hayat Ahmedjara <hayahmam3@gmail.com>
2025-06-22 12:15:58 -0700

    update page

---
+++ src/components/update.js
@@ -0,0 +1,63 @@
+import React, { useState } from "react";
+import { Link, useNavigate } from 'react-router-dom';
+import '../css/login.css'
+
+const Update = () => {
+    const [profileImage, setProfileImage] = useState(null);
+
+    const handleImageUpload = (e) => {
+        const file = e.target.files[0];
+        if (file) {
+            const imageUrl = URL.createObjectURL(file);
+            setProfileImage(imageUrl);
+        }
+    };
+    return(
+        <div className="conatiner">
+            <div className="profile-plate">
+                <div className="profile-circle">
+                    {profileImage ? (
+                        <img src={profileImage} alt="Profile" className="profile-image" />
+                    ) : (
+                        <div className="profile-placeholder"></div>
+                    )}
+                    <label className="plus-icon">
+                        <span>+</span>
+                        <input
+                            type="file"
+                            accept="image/*"
+                            onChange={handleImageUpload}
+                            style={{ display: 'none' }}
+                        />
+                    </label>
+                </div>
+            </div>
+
+            <h1>Update</h1>
+            <div className="form_group field">
+    <input type="text" className="form_field" placeholder="User Name" required />
+    <label htmlFor="name" className="form_label">User Name</label>
+    <br/>
+    <input  id="passwordinput"type="password" className="form_field" placeholder="password" required />
+    <label id="password" htmlFor="password" className="form_label">Password</label>
+    <br />
+    <input  id="emailinput" type="text" className="form_field" placeholder="email" required />
+    <label id="email" htmlFor="name" className="form_label">Email</label>
+
+    <br />
+    <input id="confirminput" type="text" className="form_field" placeholder="confirm Password" required />
+    <label id="confirm" htmlFor="name" className="form_label">Confirm Password</label>
+    
+
+</div>
+
+ 
+
+             <button className="logout_button">Sign Up</button>
+             <button className="update_button">Sign Up</button>
+            
+        </div>
+    )
+};
+
+export default Update;
---
+++ src/page/updatepage.js
@@ -0,0 +1,15 @@
+import React from "react";
+import Header from "../components/header";
+import '../css/loginpage.css';
+import Update from "../components/update";
+
+const SignUPpage=()=>{
+    return(
+        <div>
+        <Header/>
+        <Update />
+        </div>
+    )
+}
+
+export default SignUPpage;
